d95fbf06f3b1a51656eeaaa175c6adf7dcd15b9f
Daniel da Rocha <daniel.ro@browserstack.com>
2024-11-18 17:36:48 +0100

    Setup types for the Funnel object

---
+++ types/funnel.ts
@@ -0,0 +1,49 @@
+type TextBlock = {
+  id: string;
+  type: "text";
+  text: string;
+  color: string;
+  align: "left" | "center" | "right";
+};
+
+type ImageBlock = {
+  id: string;
+  type: "image";
+  src: string;
+  alt?: string;
+};
+
+type ListItem = {
+  id: string;
+  title: string;
+  description: string;
+  src: string;
+};
+
+type ListBlock = {
+  id: string;
+  type: "list";
+  items: ListItem[];
+};
+
+type ButtonBlock = {
+  id: string;
+  type: "button";
+  text: string;
+  color: string;
+  bgColor: string;
+};
+
+export type Block = TextBlock | ImageBlock | ListBlock | ButtonBlock;
+
+type FunnelPage = {
+  id: string;
+  blocks: Block[];
+};
+
+export type Funnel = {
+  id: string;
+  name: string;
+  bgColor: string;
+  pages: FunnelPage[];
+};

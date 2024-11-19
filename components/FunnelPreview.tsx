9ed9c7ae0994bc738cbbb3b82675c435de1f29b1
Daniel da Rocha <danieldarocha@MacBookPro.fritz.box>
2024-11-19 17:32:12 +0100

    Update next/previous buttons in FunnelPreview to use icons

--- components/FunnelPreview.tsx
+++ components/FunnelPreview.tsx
@@ -29,15 +29,28 @@ export default function FunnelPreview() {
   }
 
   const page = funnel.pages[currentPage];
-
   return (
-    <div className="flex flex-col items-center gap-4">
+    <div className="flex flex-col items-center">
       <div className="flex gap-4">
         <button
           onClick={() => setCurrentPage((prev) => Math.max(0, prev - 1))}
           disabled={currentPage === 0}
+          className="px-4 -mx-4 disabled:text-slate-300"
         >
-          prev
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 19.5 8.25 12l7.5-7.5"
+            />
+          </svg>
         </button>
         <PhoneFrame bgColor={funnel.bgColor}>
           <div
@@ -61,16 +74,7 @@ export default function FunnelPreview() {
             )
           }
           disabled={currentPage === funnel.pages.length - 1}
-        >
-          next
-        </button>
-      </div>
-      <div className="flex flex-col items-center gap-4">
-        <span className="font-bold">{funnel.name}</span>
-        <button
-          type="button"
-          className="flex items-center gap-2 rounded bg-white px-2 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-          onClick={() => setFunnel(null)}
+          className="px-4 -mx-4 disabled:text-slate-300"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -78,15 +82,14 @@ export default function FunnelPreview() {
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="size-4"
+            className="size-8"
           >
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
-              d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+              d="m8.25 4.5 7.5 7.5-7.5 7.5"
             />
           </svg>
-          <span>Replace funnel</span>
         </button>
       </div>
     </div>

1229c5277142fc9f090a04cde0cf26fb7096cd8c
Daniel da Rocha <daniel.ro@browserstack.com>
2024-11-20 12:38:11 +0100

    Add animation to the header

--- app/page.tsx
+++ app/page.tsx
@@ -33,11 +33,37 @@ export default function Home() {
     <FunnelContext.Provider value={[funnel, setFunnel]}>
       <div className="grid grid-rows-[1fr_auto_1fr]  justify-items-center min-h-screen p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-inter)] bg-gray-50">
         <header className="justify-items-center">
-          <Image
-            src={perspectiveLogo}
-            alt="Perspective Logo"
-            className="w-6 h-6"
-          />
+          <div className="animated-header">
+            <Image
+              src={perspectiveLogo}
+              alt="Perspective Logo"
+              className="header-logo"
+            />
+            <div className="header-title">
+              <div className="flex-grow">
+                <span className="ml-2 font-semibold text-slate-800">
+                  Funnel Viewer
+                </span>
+                <span className="ml-1 text-slate-500">by Perspective</span>
+              </div>
+              <a
+                href="https://github.com/danrocha/perspective-funnels"
+                className="ml-12 text-slate-500 hover:text-slate-800"
+                rel="noopener"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  className="size-6"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33s1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+          </div>
         </header>
         <main className="flex flex-col gap-8 row-start-2 items-center justify-center h-full">
           <div className="flex flex-col items-center min-h-[600px]">

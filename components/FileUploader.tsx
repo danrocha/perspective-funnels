a7df34cec1127f7602a05f469528fbfd9ba30f76
Daniel da Rocha <daniel.ro@browserstack.com>
2024-11-20 09:04:28 +0100

    Add loader in FileUploader

--- components/FileUploader.tsx
+++ components/FileUploader.tsx
@@ -1,22 +1,18 @@
 "use client";
-import { useCallback, useContext, useEffect } from "react";
+import { useCallback, useContext, useState } from "react";
 import { FunnelContext } from "@/lib/contexts";
 import { useDropzone } from "react-dropzone";
 import toast from "react-hot-toast";
 import PhoneFrame from "./PhoneFrame";
 
-export function FileUploader({
-  autoOpenFileInput,
-  onOpenFileInputDialog,
-}: {
-  autoOpenFileInput: boolean;
-  onOpenFileInputDialog: VoidFunction;
-}) {
-  const [funnel, setFunnel] = useContext(FunnelContext);
+export function FileUploader() {
+  const [, setFunnel] = useContext(FunnelContext);
+  const [isLoading, setIsLoading] = useState(false);
 
   const onDrop = useCallback(
     (acceptedFiles: File[]) => {
       const file = acceptedFiles[0];
+      setIsLoading(true);
       if (!file) return;
 
       const reader = new FileReader();
@@ -30,32 +26,32 @@ export function FileUploader({
             throw new Error("Invalid funnel format");
           }
           setFunnel(json);
-          toast.success("Funnel loaded successfully.");
+          localStorage.setItem("funnel", text as string);
         } catch (error) {
           toast.error(
-            "Failed to parse the funnel file. Please check the format.",
+            "Failed to parse the funnel file. Please check the file format.",
           );
+          console.error(error);
+        } finally {
+          setIsLoading(false);
         }
+      };
+      reader.onerror = () => {
+        toast.error("Error reading the file.");
+        setIsLoading(false);
       };
       reader.readAsText(file);
     },
     [setFunnel],
   );
 
-  const { getRootProps, getInputProps, isDragActive, open } = useDropzone({
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
     accept: {
       "application/json": [".json"],
     },
     maxFiles: 1,
   });
-
-  useEffect(() => {
-    if (autoOpenFileInput) {
-      open();
-      onOpenFileInputDialog();
-    }
-  }, [autoOpenFileInput, open, onOpenFileInputDialog]); // Empty dependency array to only run on mount
 
   return (
     <PhoneFrame>
@@ -64,46 +60,45 @@ export function FileUploader({
         className={`w-full h-full flex items-center transition-colors justify-center ${isDragActive ? "bg-sky-500/50" : "bg-white"}`}
       >
         <input {...getInputProps()} id="file-upload" />
-        {/* icon */}
-        <div className="text-center min-h-1/2 grid grid-cols-1 grid-rows-2 gap-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className={`mx-auto size-12 ${isDragActive ? "text-white" : "text-sky-500"}`}
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z"
-            />
-          </svg>
-          {/* input  */}
+        {isLoading ? (
+          <div className="text-center">
+            <div className="loader mx-auto"></div>
+            <p className="mt-4 text-sm text-gray-600">Loading funnel...</p>
+          </div>
+        ) : (
+          <button className="text-center min-h-1/2 grid grid-cols-1 grid-rows-2 gap-4 hover:brightness-75">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className={`mx-auto size-12 ${isDragActive ? "text-white" : "text-sky-500"}`}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z"
+              />
+            </svg>
 
-          <div className="text-sm/6 ">
-            {isDragActive ? (
-              <p className="font-semibold text-white">Drop the file here.</p>
-            ) : (
-              <>
-                <label
-                  htmlFor="file-upload"
-                  className="relative cursor-pointer rounded-md bg-white  text-sky-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-sky-600 focus-within:ring-offset-2 hover:text-sky-500"
-                  onClick={(event) => event.stopPropagation()}
-                >
-                  <span className="font-semibold">
+            <div className="text-sm/6 ">
+              {isDragActive ? (
+                <p className="font-semibold text-white">Drop the file here.</p>
+              ) : (
+                <>
+                  <span className="font-semibold text-sky-500">
                     Upload a your funnel JSON file
                   </span>
                   <br />
-                  <span className="text-gray-600">
+                  <span className="text-slate-600">
                     or drag and drop it here.
                   </span>
-                </label>
-              </>
-            )}
-          </div>
-        </div>
+                </>
+              )}
+            </div>
+          </button>
+        )}
       </div>
     </PhoneFrame>
   );
